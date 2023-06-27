22f0ba47d3dbee070d1fd850b3ef2d8da9eca2cc
Ljubica Cimeša <ljubica994@gmail.com>
2023-06-27 10:53:49 +0200

    Add files via upload

---
+++ 6_Input_Driven_SNN_regimes/A,B,C/Fig5_SI.py
@@ -0,0 +1,383 @@
+from Spiking_rank_one_INPUTS_C import *
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pickle
+import matplotlib.gridspec as gridspec
+
+def firing_rates(spikes_t_all = np.zeros(0), spikes_i_all = np.zeros(0), time_array = np.zeros(0), tau_s = 100*ms, N_total = 1000):
+
+    n_steps_rates = len(time_array)
+    aver_rates = np.zeros((n_steps_rates, N_total))
+    N_trials = len(spikes_t_all)
+    print(N_trials)
+    dt = 1*ms
+    sd_firing = []
+    rates_all = []
+    print(time_array)
+    for trial in range(0, N_trials):
+        print('Iteracija: '+str(trial))
+        spike_t = spikes_t_all[trial]*second
+        spike_i = spikes_i_all[trial]
+        inst_rates = np.zeros((len(time_array), N_total))
+
+        for k in range(n_steps_rates-1):
+            t_k = time_array[k]
+            t_k_plus_one = time_array[k+1]
+            #list of neurons that spiked between t_k and t_k_plus_one
+            spiking_neurons = spike_i[(t_k <= spike_t) & (spike_t<t_k_plus_one)]
+            # print(spiking_neurons)
+            for i in range(N_total):
+                if i in spiking_neurons : ######################§#############################
+                    inst_rates[k+1][i] = inst_rates[k][i]*(1-dt/tau_s) + dt/tau_s
+                else: ######################################################################
+                    inst_rates[k+1][i] = inst_rates[k][i]*(1-dt/tau_s)
+                # if (i==0): print(inst_rates[k+1][0])
+
+        inst_rates = 1000*inst_rates
+        rates_all.append(inst_rates)
+        # inst_rates_all.append(inst_rates)
+        aver_rates = (aver_rates*trial + inst_rates)/(trial+1)
+        pop_rate = np.mean(aver_rates, axis=1)
+
+        sd_firing.append(np.std(pop_rate))
+
+    return [rates_all, aver_rates, sd_firing]
+
+H = 3
+W = 5
+# W = 7.2 the one at overleaf in the moment
+
+plt.rcParams['figure.figsize'] = W,H
+plt.rcParams['font.size'] = 24
+# plt.rcParams["font.family"] = "serif"
+# "font.sans-serif": ["Helvetica"] an example from matplotlib documentation
+plt.rcParams['lines.linewidth'] = 2
+# 8 fmar
+# plt.rcParams['figure.figsize'] = 4,4
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+
+# ======== Firing rates ========
+factor_mn = 1
+
+I_e = 15*mV
+I_o = 1*mV*100
+sigma = 0.1
+mu_noise = 80
+
+J_syn = 0.1*mV
+
+Sm = 2
+Sn = 2*0.01
+
+g = 6
+# =============== filter time constant ==========
+tau_D_min = 1.5
+tau_D_max = 1.5
+
+N_trials = 1
+#  for 10000 neurons,  5 trials
+syn_Del = 'const'
+c_arr = np.asarray([0.14])
+c_arr = np.asarray([0.5])
+#  possible for inp_D which is input direction : 'const', 'along' or 'rand'
+color_a = 'steelblue'
+color_o = 'gray'
+color_e = 'purple'
+
+N = 12500
+C_sparsity = int(0.1*N)
+# C=C_sparsity,
+# set generate_res to True
+generate_res = False
+tau_short = 1*ms
+tau_long = 100*ms
+t_run = 2*second
+tau_all = np.asarray([1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])*ms
+# tau_all = np.asarray([1,100])*ms
+t_arr = np.linspace(0, t_run, int(t_run/0.001))*second
+raster_trials_plot = [0, 0, 0]
+filename = 'RESULTS'
+# filename= 'Diff_mn_Results_N=1000, J =0.5 mV, N_trials=100, tau_short=10. ms, tau_long=100. ms'
+if generate_res:
+
+    spikes_res_along = SPIKES_C(Sm = Sm, Sn = Sn, J_value =J_syn, factor_mn = factor_mn, I_ampl = I_o, ind_c = 0, c_array = c_arr,  mu_noise = mu_noise*mV,
+                    sigma_noise = sigma*mV, nmb_trials = N_trials, N_total= N, C=C_sparsity,  syn_D = syn_Del, inp_D = 'along',
+                    tau_s = tau_long, tau_min = tau_D_min, tau_max = tau_D_max, inhib_g = g, seed_start=1, t_run = t_run )
+
+    spikes_res_orth = SPIKES_C(Sm = Sm, Sn = Sn, J_value = J_syn, factor_mn = factor_mn, I_ampl =  I_o, ind_c = 0, c_array = c_arr, mu_noise = mu_noise*mV,
+                    sigma_noise = sigma*mV, nmb_trials = N_trials, N_total= N, C=C_sparsity, syn_D = syn_Del, inp_D = 'orth',
+                    tau_s= tau_long, tau_min = tau_D_min, tau_max = tau_D_max, inhib_g = g, seed_start=2, t_run =t_run)
+
+    spikes_res_equal = SPIKES_C(Sm = Sm, Sn = Sn, J_value = J_syn, factor_mn = factor_mn, I_ampl = I_e, ind_c = 0, c_array = c_arr, mu_noise = mu_noise*mV,
+                    sigma_noise = sigma*mV, nmb_trials = N_trials, N_total= N, C=C_sparsity, syn_D = syn_Del, inp_D = 'const',
+                    tau_s= tau_long, tau_min = tau_D_min, tau_max = tau_D_max, inhib_g = g, seed_start=3, t_run = t_run)
+
+
+    f = open(filename, 'wb')
+    I_dir = ['along', 'orth', 'equal']
+    spikes_res = [spikes_res_along, spikes_res_orth, spikes_res_equal]
+    for d in range(0,3):
+
+        S = {}
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; spikes_t'] = spikes_res[d]['spike_times']
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; spikes_i'] = spikes_res[d]['neuron ind.']
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; vector_I'] = spikes_res[d]['vector_I']
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; vector_m'] = spikes_res[d]['vector_m']
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; vector_n'] = spikes_res[d]['vector_n']
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; input_I'] = spikes_res[d]['I input'][0]
+        S['I_'+I_dir[d]+'; J='+str(J_syn)+'; t_arr'] = spikes_res[d]['t_arr']
+
+
+        pickle.dump(S, f)
+
+    f.close()
+
+    spikes_t_a = spikes_res_along['spike_times']
+    spikes_i_a = spikes_res_along['neuron ind.']
+    print('soikes res along t_arr:')
+    print(spikes_res_along['t_arr'])
+    R1 = {}
+    rates_1_tau_all = []
+
+    for tau in tau_all:
+        rates_1_tau = firing_rates(spikes_t_a, spikes_i_a, spikes_res_along['t_arr'], tau, N_total=N)
+        rates_1_tau_all.append(rates_1_tau)
+
+
+    spikes_t_o = spikes_res_orth['spike_times']
+    spikes_i_o = spikes_res_orth['neuron ind.']
+
+    rates_2_tau_all = []
+    for tau in tau_all:
+        rates_2_tau = firing_rates(spikes_t_o, spikes_i_o, spikes_res_orth['t_arr'], tau, N_total=N)
+        rates_2_tau_all.append(rates_2_tau)
+
+    spikes_t_e = spikes_res_equal['spike_times']
+    spikes_i_e = spikes_res_equal['neuron ind.']
+
+    rates_3_tau_all = []
+    for tau in tau_all:
+        rates_3_tau = firing_rates(spikes_t_e, spikes_i_e, spikes_res_equal['t_arr'], tau, N_total=N)
+        rates_3_tau_all.append(rates_3_tau)
+
+    rates_all = [rates_1_tau_all, rates_2_tau_all, rates_3_tau_all]
+
+    f = open(filename, 'ab')
+
+    for k in range(0,3):
+        R={}
+        R['rates_' +I_dir[k]+ '; J='+str(J_syn)+'; tau_all'] = rates_all[k]
+        pickle.dump(R,f)
+    f.close()
+
+# t1 = 0.75
+# t2 = 1.25
+
+t1 = 0.9
+t_tr = 0.7
+t2 = 1.1
+tau_choice = 100*ms
+# t = spikes_res_along['time']
+t = np.linspace(0,5,5000)
+np.random.seed(1)
+ind1 = int(t1*1000)
+ind2 = int(t2*1000)
+ind_tr = int(t_tr*1000)
+ind_rate1 = np.argwhere(tau_all == 1*ms)[0][0]
+ind_rate2 = np.argwhere(tau_all == 100*ms)[0][0]
+
+
+# ======================= RASTERS ==========================
+color1 = 'steelblue'
+color2 = 'coral'
+
+f1 = plt.figure()
+f2 = plt.figure()
+f3 = plt.figure()
+f4 = plt.figure()
+f5 = plt.figure()
+f6 = plt.figure()
+f7 = plt.figure()
+f8 = plt.figure()
+
+
+
+f = open(filename, 'rb')
+S1 = pickle.load(f)
+spikes_t_a = S1['I_along; J='+str(J_syn)+'; spikes_t']
+spikes_i_a = S1['I_along; J='+str(J_syn)+'; spikes_i']
+vector_I_a = S1['I_along; J='+str(J_syn)+'; vector_I']
+vector_m_a = S1['I_along; J='+str(J_syn)+'; vector_m']
+vector_n_a = S1['I_along; J='+str(J_syn)+'; vector_n']
+t_arr_a = S1['I_along; J='+str(J_syn)+'; t_arr']
+
+S2 = pickle.load(f)
+spikes_t_o = S2['I_orth; J='+str(J_syn)+'; spikes_t']
+spikes_i_o = S2['I_orth; J='+str(J_syn)+'; spikes_i']
+vector_I_o = S2['I_orth; J='+str(J_syn)+'; vector_I']
+vector_m_o = S2['I_orth; J='+str(J_syn)+'; vector_m']
+vector_n_o = S2['I_orth; J='+str(J_syn)+'; vector_n']
+t_arr_o = S2['I_orth; J='+str(J_syn)+'; t_arr']
+
+S3 = pickle.load(f)
+spikes_t_e = S3['I_equal; J='+str(J_syn)+'; spikes_t']
+spikes_i_e = S3['I_equal; J='+str(J_syn)+'; spikes_i']
+vector_I_e = S3['I_equal; J='+str(J_syn)+'; vector_I']
+vector_m_e = S3['I_equal; J='+str(J_syn)+'; vector_m']
+vector_n_e = S3['I_equal; J='+str(J_syn)+'; vector_n']
+t_arr_e = S3['I_equal; J='+str(J_syn)+'; t_arr']
+
+
+R1 = pickle.load(f)
+R2 = pickle.load(f)
+R3 = pickle.load(f)
+
+f.close()
+
+# ====
+plt.figure(1)
+cond = np.logical_and(np.logical_and(spikes_t_a>t1, spikes_t_a<t2), spikes_i_a<30)
+plt.plot(spikes_t_a[cond], spikes_i_a[cond],'.', color = 'k')
+
+# ====
+
+rates_a1 = R1['rates_along; J='+str(J_syn)+'; tau_all'][ind_rate1]
+rates_a2 = R1['rates_along; J='+str(J_syn)+'; tau_all'][ind_rate2]
+
+
+plt.figure(2)
+# plt.plot(t_arr[ind1:ind2], np.mean(rates_a_tau_short[1][ind1:ind2,:], axis=1 ), color = color_a)
+
+plt.plot(t_arr_a[ind1:ind2], np.mean(rates_a1[1][ind1:ind2,:], axis=1 ), color = color1)
+plt.plot(t_arr_a[ind1:ind2], np.mean(rates_a2[1][ind1:ind2,:], axis=1 ), color = color2)
+plt.yticks([0,200])
+
+# =======ALONG
+
+proj_I_a1 = np.dot(rates_a1[0][0], vector_I_a[0])/np.linalg.norm( vector_I_a[0])
+proj_m_a1 = np.dot(rates_a1[0][0], vector_m_a[0])/np.linalg.norm(vector_m_a[0])
+
+
+proj_I_a2 = np.dot(rates_a2[0][0], vector_I_a[0])/np.linalg.norm(vector_I_a[0])
+proj_m_a2 = np.dot(rates_a2[0][0], vector_m_a[0])/np.linalg.norm(vector_m_a[0])
+
+plt.figure(3)
+plt.plot(proj_I_a1[ind_tr:], proj_m_a1[ind_tr:], color = color1)
+plt.plot(proj_I_a2[ind_tr:], proj_m_a2[ind_tr:], color = color2)
+
+
+
+# ========= PCA ALONG =======
+
+Proj_global = []
+Proj_m_all = []
+for tau in tau_all:
+    ind_rate = np.argwhere(tau_all == tau)[0][0]
+    rates_a = R1['rates_along; J='+str(J_syn)+'; tau_all'][ind_rate]
+
+    X = rates_a[1][ind_tr:,:] - np.mean(rates_a[1][ind_tr:,:], axis = 0)
+
+    # X = rates_a_tau_short[1] - np.mean(rates_a_tau_short[1], axis = 0)
+    C = np.cov(X.transpose())  # Compute covariance matrix
+    C = np.dot(X.T, X) / (X.shape[0] - 1)
+
+    Lambda, eigvec = np.linalg.eig(C)
+    Lambda = Lambda.real
+    eigvec = eigvec.real
+
+    Lambda_sort = Lambda[(np.argsort(-Lambda))]
+    E_a = eigvec[:,np.argsort(-Lambda)]
+
+    X_prim = np.dot(X,E_a)
+    C_prim = np.dot(X_prim.transpose(), X_prim)
+
+    PCA_a = []
+    for i in range(0,8):
+        PCA_a.append(C_prim[i,i]/np.trace(C_prim))
+
+
+    # ======== PROJECTINGS VECTORS m, n and global axis onto first PCAs
+
+    vector_g = np.ones(len(vector_m_a[0]))
+    g_norm = vector_g/np.linalg.norm(vector_g)
+    I_norm_a = vector_I_a[0]/np.linalg.norm(vector_I_a[0])
+    m_norm_a = vector_m_a[0]/np.linalg.norm(vector_m_a[0])
+
+    Proj_global.append(np.absolute(np.dot(g_norm, E_a[:,0])))
+    Proj_m_all.append(np.absolute(np.dot(m_norm_a, E_a[:,0])))
+
+    if tau == 1*ms:
+        plt.figure(4)
+        plt.bar(np.linspace(0,8,8), PCA_a, color = color1)
+        plt.yticks([0,0.05])
+
+        PCA_projs_a = []
+
+        for i in range(0,3):
+            PCA_g = np.dot(g_norm, E_a[:,i])
+
+            PCA_I = np.dot(I_norm_a, E_a[:,i])
+
+            PCA_m = np.dot(m_norm_a, E_a[:,i])
+
+            PCA_projs_a.append([PCA_g, PCA_I, PCA_m])
+
+        PCA_projs = np.absolute(np.asarray(PCA_projs_a))
+        IM1 = PCA_projs.transpose()
+
+        plt.figure(5)
+        plt.imshow(IM1, cmap = 'Greys')
+        print('min 1 = '+str(np.min(IM1)))
+        print('max 1 = '+str(np.max(IM1)))
+        plt.clim([0,1])
+
+        plt.colorbar()
+        plt.xticks([0,1,2])
+        plt.yticks([])
+
+    if tau == 100*ms:
+        plt.figure(6)
+        plt.bar(np.linspace(0,8,8), PCA_a, color = color2)
+        plt.yticks([0,1])
+
+        PCA_projs_a = []
+        for i in range(0,3):
+            PCA_g = np.dot(g_norm, E_a[:,i])
+
+            PCA_I = np.dot(I_norm_a, E_a[:,i])
+
+            PCA_m = np.dot(m_norm_a, E_a[:,i])
+
+            PCA_projs_a.append([PCA_g, PCA_I, PCA_m])
+
+        PCA_projs = np.absolute(np.asarray(PCA_projs_a))
+        IM1 = PCA_projs.transpose()
+        print('min 100 = '+str(np.min(IM1)))
+        print('max 100 = '+str(np.max(IM1)))
+
+        plt.figure(7)
+        plt.imshow(IM1, cmap = 'Greys')
+        plt.colorbar()
+        plt.clim([0,1])
+        plt.xticks([0,1,2])
+        plt.yticks([])
+
+
+plt.figure(8)
+plt.plot(tau_all, Proj_global, color = 'k')
+plt.plot(tau_all, Proj_m_all, color = 'g')
+
+plt.yticks([0,1])
+
+
+
+
+f1.set_tight_layout(True)
+f2.set_tight_layout(True)
+f3.set_tight_layout(True)
+f4.set_tight_layout(True)
+f5.set_tight_layout(True)
+f6.set_tight_layout(True)
+f7.set_tight_layout(True)
+f8.set_tight_layout(True)
---
+++ 6_Input_Driven_SNN_regimes/D,E,F/Spiking_rank_one_INPUTS_C.py
@@ -0,0 +1,303 @@
+import numpy as np
+from brian2 import Equations, NeuronGroup, Synapses, Network, TimedArray, SpikeMonitor, StateMonitor, PopulationRateMonitor, \
+    seed, second, mV, ms, volt, set_device, device, defaultclock
+import random
+import matplotlib.pyplot as plt
+import pickle
+import os
+import scipy
+import time
+# set_device('cpp_standalone', directory='EB_standalone')
+# defaultclock.dt = 0.05*ms
+filename='values_RATES_mn_different_means_std=1_'
+identification='m_second'
+
+plt.rcParams['font.size']=14
+plt.rcParams['figure.figsize']= 4, 2
+plt.rcParams['legend.fontsize'] = 8
+plt.rcParams['lines.markersize'] = 4
+# defaultclock.dt = 0.1*ms
+
+def SPIKES_C(Sm = 2, Sn = 2, J_value = 0.6*mV, factor_mn = 0.01, I_ampl = 1, ind_c = 1, c_array = np.linspace(0,0.2,11), mu_noise = 24*mV,
+             sigma_noise = 0.1*mV, nmb_trials = 10, N_total = 1000, C = 100,  syn_D = 'const', inp_D = 'const',
+             tau_s = 100*ms, tau_min = 0.55, tau_max = 1.55, inhib_g = 5, t_I_ramp = 0, seed_start = 0, t_run = 5*second):
+
+    print(J_value)
+    spikes_dict = {}
+    # spikes_dict['c_array'] = c_array
+    spikes_dict['J'] = J_value
+    # seed for vectors m and n and matrices E_I
+    # seed0 = 1
+    # np.random.seed(seed=seed0)
+    # random.seed(seed0)
+    # seed(seed0)
+    np.random.seed(seed=seed_start)
+    random.seed(seed_start)
+    seed(seed_start)
+#=============== NUMBER OF NEURONS: TOTAL, EXCITATORY, INHIBITORY ========================
+    N_exc = int(0.8 * N_total)
+    N_inh = int(0.2 * N_total)
+    f = C/N_total
+    # f = 0.1 # sparsity
+    C_e = int(f * N_exc)
+    C_i = int(f * N_inh)
+    # C = C_e + C_i
+#================================= MATRIX CHI ==================================
+
+
+#============================= NOISE PART =======================================
+    mu_noise = mu_noise
+    sigma_noise = sigma_noise
+
+    t_beg = 1*second
+    tau_m = 20*ms #
+    t_ref = 0.5*ms
+
+    # sigma_noise_new = sigma_noise/np.sqrt(2*tau_m)
+    # t_ref= 0*ms
+
+    V_th = 20*mV
+    V_r = 10*mV
+#=========================== EXTERNAL INPUT PART ===============================
+    g = inhib_g
+    device.reinit()
+    device.activate()
+# ========================== STRUCTURE VECTORS =========================
+#     Sm = 2 now in the input of the function
+#     Sn = 2
+    Smn = 0
+#==================== RANK-ONE MATRIX AND VECTORS ==========================
+    ones = np.ones(N_total)
+    m_1, n_1 = np.random.multivariate_normal([0,0],[[Sm**2, Smn],[Smn, Sn**2]],N_total).transpose()
+    m_1 = m_1 - (np.dot(m_1,ones)/np.dot(ones,ones))*ones
+    n_1 = n_1 - (np.dot(n_1,ones)/np.dot(ones,ones))*ones
+    I_dir_orth = np.random.normal(0,1,N_total)
+    I_dir_orth = I_dir_orth - (np.dot(I_dir_orth, ones)/np.dot(ones, ones))*ones - \
+        (np.dot(I_dir_orth, m_1)/np.dot(m_1, m_1))*m_1 - (np.dot(I_dir_orth, n_1)/np.dot(n_1, n_1))*n_1
+    I_dir_orth = Sn*I_dir_orth
+
+
+
+#============== RANK 2 ========================
+    # E-I matrix
+    E_I = np.zeros((N_total, N_total))
+    for i in range(0, N_total):
+        ind_exc = random.sample(range(0, N_exc), C_e)
+        ind_inh = random.sample(range(N_exc, N_inh+N_exc), C_i)
+        E_I[i, ind_exc] = J_value
+        E_I[i, ind_inh] = -g*J_value
+    m_2 = np.ones(N_total)
+    n_2 = np.zeros(N_total)
+    n_2[:N_exc] = J_value*C_e/N_exc
+    n_2[N_exc:] = -g*J_value*C_i/N_inh
+
+    print('factor mn=')
+    print(factor_mn)
+
+#     rank_1 = factor_mn * np.outer(m_1, n_1)/N_total
+#     # rank_2 is already in the E_I matrix, that is the mean part we subtract from E_I matrix
+#
+#     J = E_I + rank_1
+#     J = J*volt
+#     print(J)
+# # ======================================================================
+#     dt = 1*ms
+#
+# # ================ Input I in time ================
+#     if inp_D == 'const':
+#         print('Input is along (1,1..)')
+#         I_vector = np.zeros(N_total) + 1
+#         spikes_dict['vector_I'] = I_vector
+#
+#
+#     elif inp_D == 'along':
+#         print('Input is along n')
+#         I_vector = n_1
+#         spikes_dict['vector_I'] = n_1
+#
+#     elif inp_D == 'orth':
+#         print('Input is orthogonal to n')
+#         I_vector = I_dir_orth
+#         spikes_dict['vector_I'] = I_dir_orth
+#     else:
+#         print('Input is not chosen properly')
+#
+#     t_ramp = 0*second # 0.5 POSTAVLJENO ZA RAMPU
+#     I_input = np.zeros((int(t_run/dt), N_total))
+#     for tk in range(0, int((t_beg-t_ramp)/dt)):
+#         I_input[tk,:] = 0*I_vector
+#     for tk in range(int((t_beg-t_ramp)/dt), int((t_beg+t_ramp)/dt)):
+#         I_input[tk,:] = I_vector*( dt*tk/second - t_ramp/second )
+#     for tk in range(int((t_beg+t_ramp)/dt), int(t_run/dt)):
+#         I_input[tk,:] = I_vector
+#     I_input = I_input*I_ampl
+#
+    dt = 1*ms # ?
+
+    n_steps_rates = int(t_run/dt)
+
+# different seeds for each trial
+# different seeds for membrane potentials
+
+
+    b = 0
+    seed_array = []
+    while b < N_total:
+        s_sample = np.random.randint(0, 20000, 1)
+        if not (s_sample in seed_array):
+            seed_array.append(s_sample)
+            b+=1
+
+    seed_array = np.concatenate(seed_array)
+
+    Inputs_all = []
+    spikes_t_all = []
+    spikes_i_all = []
+
+    m_vectors_all = []
+    n_vectors_all = []
+    I_vectors_all = []
+
+    for c in c_array:
+        print(c)
+
+        for iter in range(0, nmb_trials):
+
+            seed1 = seed_array[iter]
+            print('Iteracija :'+str(iter))
+            np.random.seed(seed=seed1)
+            random.seed(seed1)
+            seed(seed1)
+
+            # m_1, n_1 = np.random.multivariate_normal([0,0],[[Sm**2, Smn],[Smn, Sn**2]],N_total).transpose()
+            # m_1 = m_1 - (np.dot(m_1,ones)/np.dot(ones,ones))*ones
+            # n_1 = n_1 - (np.dot(n_1,ones)/np.dot(ones,ones))*ones
+            # I_dir_orth = np.random.normal(0,1,N_total)
+            # I_dir_orth = I_dir_orth - (np.dot(I_dir_orth, ones)/np.dot(ones, ones))*ones - \
+            #     (np.dot(I_dir_orth, m_1)/np.dot(m_1, m_1))*m_1 - (np.dot(I_dir_orth, n_1)/np.dot(n_1, n_1))*n_1
+            # I_dir_orth = 2*I_dir_orth
+
+            rank_1 = factor_mn * np.outer(m_1, n_1)/N_total
+    # rank_2 is already in the E_I matrix, that is the mean part we subtract from E_I matrix
+
+            J = E_I + rank_1
+            J = J*volt
+            # print(J)
+        # ======================================================================
+            dt = 1*ms
+
+        # ================ Input I in time ================
+            if inp_D == 'const':
+                print('Input is along (1,1..)')
+                I_vector = np.zeros(N_total) + 1
+
+            elif inp_D == 'along':
+                print('Input is along n')
+                I_vector = n_1
+
+            elif inp_D == 'orth':
+                print('Input is orthogonal to n')
+                I_vector = I_dir_orth
+            else:
+                print('Input is not chosen properly')
+
+
+            m_vectors_all.append(m_1)
+            n_vectors_all.append(n_1)
+            I_vectors_all.append(I_vector)
+
+
+            t_ramp = 0*second # 0.5 POSTAVLJENO ZA RAMPU
+            I_input = np.zeros((int(t_run/dt), N_total))
+            try:
+                for tk in range(0, int((t_beg-t_ramp)/dt)):
+                    I_input[tk,:] = 0*I_vector
+                for tk in range(int((t_beg-t_ramp)/dt), int((t_beg+t_ramp)/dt)):
+                    I_input[tk,:] = I_vector*( dt*tk/second - t_ramp/second )
+                for tk in range(int((t_beg+t_ramp)/dt), int(t_run/dt)):
+                    I_input[tk,:] = I_vector
+            except:
+                print('no input')
+            I_input = I_input*I_ampl
+
+
+            Inputs_all.append(c*I_input[-1,:])
+
+            dt = 1*ms # ?
+
+            V_r_init = V_r + np.random.normal(0, 0.1, N_total)*V_r
+
+            RI_ext = TimedArray(c*I_input, dt=dt)
+            #=================================== NEURON MODEL ======================================
+            eqs = Equations(
+
+                ''' dv/dt = (-v +  RI_ext(t,i) + mu_noise + sigma_noise * xi* second**0.5) /tau_m : volt (unless refractory)''',
+                tau_m=tau_m)
+
+            neurons = NeuronGroup(\
+                        N_total,
+                        model = eqs,
+                        threshold = 'v > V_th',
+                        reset='v = V_r',
+                        refractory = t_ref,
+                        name='neurons',
+                        method='euler'
+                        )
+
+            neurons.v = V_r_init
+
+            synapses = Synapses(neurons, neurons,
+                               model='''connectivity_matrix : volt
+                               ''',
+                               on_pre='''v_post += connectivity_matrix''',
+
+                               # delay = 100*ms,
+                               name='synapses')
+
+            synapses.connect()
+            synapses.connectivity_matrix = (J.transpose()).flatten()
+
+    #======================= setting up delays
+            synaptic_delay = np.zeros((1, N_total*N_total))
+            if syn_D == 'const':
+                synaptic_delay = synaptic_delay + tau_min
+                print('const delays')
+            else:
+                print('rand delays')
+                for k in range(0,N_total):
+                    # synaptic_delay[k,:] = np.linspace(0.55, 1.55, N_total)
+                    synaptic_delay[:,k*N_total:(k+1)*N_total] = tau_min + k*(tau_max-tau_min)/(N_total-1)
+            print(synaptic_delay)
+            synapses.delay = synaptic_delay*ms
+
+    #----------------------- RASTER PLOTS -----------------------------------------------
+            spikes = SpikeMonitor(neurons, name='spikes_monitor')
+            # syn_mon = StateMonitor(synapses,['connectivity_matrix'], record = range(0,N_total), name='syn_monitor', dt=100*ms)
+            # V_mon = StateMonitor(neurons,'v', record=range(0,5), name='V_monitor', dt=1*ms)
+            #  syn_monitor and V monitor removed, too much memory
+            net = Network(neurons, synapses, spikes)
+            net.run(t_run, report='text')
+
+
+    # ======================= deo iz Lazarevog koda, beginning =======================================
+
+            if c ==c_array[ind_c]:
+                spikes_t_all.append(np.asarray(spikes.t))
+                spikes_i_all.append(np.asarray(spikes.i))
+
+            device.delete()
+
+    spikes_dict['spike_times'] = np.asarray(spikes_t_all)
+    spikes_dict['neuron ind.'] = np.asarray(spikes_i_all)
+
+    spikes_dict['vector_m'] = m_vectors_all
+    spikes_dict['vector_n'] = n_vectors_all
+    spikes_dict['vector_I'] = I_vectors_all
+
+    spikes_dict['I input'] = Inputs_all
+
+    spikes_dict['t_arr'] = np.linspace(0, t_run, int(t_run/dt))
+    print('t_arr:')
+    print(spikes_dict['t_arr'])
+
+    return spikes_dict
